b9a879381ef6e8100ad56f2f7f3148ed68f8fb1f
Ifiok Ekott <ekottifiok@gmail.com>
2024-07-30 07:47:38 +0100

    added main.h file for the main.c

---
+++ server/main.h
@@ -0,0 +1,20 @@
+
+struct StartupArgs
+{
+    std::string strMode;
+    std::string strPrimaryInterface;
+    std::string strAltInterface;
+    std::string strPrimaryPort;
+    std::string strAltPort;
+    std::string strPrimaryAdvertised;
+    std::string strAlternateAdvertised;
+    std::string strFamily;
+    std::string strProtocol;
+    std::string strHelp;
+    std::string strVerbosity;
+    std::string strMaxConnections;
+    std::string strDosProtect;
+    std::string strConfigFile;
+    std::string strReuseAddr;
+
+};
